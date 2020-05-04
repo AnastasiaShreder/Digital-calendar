17e299c47ec562880933f4d0ee37237af6567ba7
atagiev <andrej-tagiev@yandex.ru>
2020-05-04 16:39:25 +0300

    Новые цвета и новые задачи

--- Code/backend/test_server.py
+++ Code/backend/test_server.py
@@ -11,7 +11,42 @@ cors = CORS(app)
 #conn = pms.connect(user = "root",passwd = "0000", host = "localhost", database = "EdgePoint")
 #cursor = conn.cursor()
 
-
+tasks = [{"eventName":"Конференция 1", "calendar":"Конференция", "color":"orange",    "date":"2020-05-01"},
+         {"eventName":"Форум 1",       "calendar":"Форум",       "color":"red",       "date":"2020-05-02"},
+         {"eventName":"Фестиваль 1",   "calendar":"Фестиваль",   "color":"purple",    "date":"2020-05-10"},
+         {"eventName":"Встреча 1",     "calendar":"Встреча",     "color":"green",     "date":"2020-05-12"},
+         {"eventName":"Совещание 1",   "calendar":"Совещание",   "color":"yellow",    "date":"2020-05-23"},
+         {"eventName":"Заказ 1",       "calendar":"Заказ",       "color":"blue",      "date":"2020-05-12"},
+         {"eventName":"Прочее 1",      "calendar":"Прочее",      "color":"lightblue", "date":"2020-05-21"},
+         {"eventName":"Конференция 2", "calendar":"Конференция", "color":"orange",    "date":"2020-05-17"},
+         {"eventName":"Форум 2",       "calendar":"Форум",       "color":"red",       "date":"2020-05-09"},
+         {"eventName":"Фестиваль 2",   "calendar":"Фестиваль",   "color":"purple",    "date":"2020-05-11"},
+         {"eventName":"Встреча 2",     "calendar":"Встреча",     "color":"green",     "date":"2020-05-04"},
+         {"eventName":"Совещание 2",   "calendar":"Совещание",   "color":"yellow",    "date":"2020-05-28"},
+         {"eventName":"Заказ 2",       "calendar":"Заказ",       "color":"blue",      "date":"2020-05-25"},
+         {"eventName":"Прочее 2",      "calendar":"Прочее",      "color":"lightblue", "date":"2020-05-29"},
+         {"eventName":"Конференция 3", "calendar":"Конференция", "color":"orange",    "date":"2020-05-20"},
+         {"eventName":"Форум 3",       "calendar":"Форум",       "color":"red",       "date":"2020-05-05"},
+         {"eventName":"Фестиваль 3",   "calendar":"Фестиваль",   "color":"purple",    "date":"2020-05-24"},
+         {"eventName":"Встреча 3",     "calendar":"Встреча",     "color":"green",     "date":"2020-05-17"},
+         {"eventName":"Совещание 3",   "calendar":"Совещание",   "color":"yellow",    "date":"2020-05-13"},
+         {"eventName":"Заказ 3",       "calendar":"Заказ",       "color":"blue",      "date":"2020-05-26"},
+         {"eventName":"Прочее 3",      "calendar":"Прочее",      "color":"lightblue", "date":"2020-05-23"},
+         {"eventName":"Конференция 4", "calendar":"Конференция", "color":"orange",    "date":"2020-05-13"},
+         {"eventName":"Форум 4",       "calendar":"Форум",       "color":"red",       "date":"2020-05-07"},
+         {"eventName":"Фестиваль 4",   "calendar":"Фестиваль",   "color":"purple",    "date":"2020-05-14"},
+         {"eventName":"Встреча 4",     "calendar":"Встреча",     "color":"green",     "date":"2020-05-18"},
+         {"eventName":"Совещание 4",   "calendar":"Совещание",   "color":"yellow",    "date":"2020-05-19"},
+         {"eventName":"Заказ 4",       "calendar":"Заказ",       "color":"blue",      "date":"2020-05-20"},
+         {"eventName":"Прочее 4",      "calendar":"Прочее",      "color":"lightblue", "date":"2020-05-21"},
+         {"eventName":"Конференция 5", "calendar":"Конференция", "color":"orange",    "date":"2020-05-22"},
+         {"eventName":"Форум 5",       "calendar":"Форум",       "color":"red",       "date":"2020-05-23"},
+         {"eventName":"Фестиваль 5",   "calendar":"Фестиваль",   "color":"purple",    "date":"2020-05-24"},
+         {"eventName":"Встреча 5",     "calendar":"Встреча",     "color":"green",     "date":"2020-05-25"},
+         {"eventName":"Совещание 5",   "calendar":"Совещание",   "color":"yellow",    "date":"2020-05-26"},
+         {"eventName":"Заказ 5",       "calendar":"Заказ",       "color":"blue",      "date":"2020-05-27"},
+         {"eventName":"Прочее 5",      "calendar":"Прочее",      "color":"lightblue", "date":"2020-05-30"},
+         ]
 
 
 
@@ -33,7 +68,7 @@ def create_data(data):
         #   second_user_id - id коллеги, на которого тыкнул пользователь
         #
         #свой user_id, id проекта, id юзера 
-        return json.dumps([{"eventName":"Задача 1", "calendar":"Встреча", "color":"blue","date":"2020-04-27"},{"eventName":"Задача 3", "calendar":"Совещание", "color":"orange","date":"2020-04-04"}])
+        return json.dumps(tasks)
     
     
     if data["type"] == "login":
